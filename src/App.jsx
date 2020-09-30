dca0dd0333ff4af1229e2b09c387529706aaf492
Asif Ahmed <64367845+AsifAhmed786@users.noreply.github.com>
2020-09-30 22:11:59 +0500

    committed

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import {Header, Header2} from './component/Header.js'
+import { Header, Header2 } from './components/Header.js'
 
 // function App() {
 //   return (
@@ -11,39 +10,68 @@ import {Header, Header2} from './component/Header.js'
 //       Hello world      
 //       </h2>
 //       <Footer />
-      
+
 //     </div>
 //   );
 // }
 
-class App extends React.Component{
-  render(){
-    return(
-      <div className="body">
-       <Header />
-       <h2>
-       Hello world      
-       </h2>
-       <Footer />      
-     </div>
-    )
+class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      name: "Asif",
+      email: "asifahmed1us@hotmail.com"
+    }
   }
-}
+  getState = () => {
+    console.log(this.state.name)
+  }
+
+  set_state = (e) => {
+    this.setState({
+      name: "Asif Ahmed1"
+    })
+  }
+
+  handleChange = (e)=>{
+    this.setState({
+      [e.target.name] : e.target.value,
+      
+    })
+  }
+
+  get_props(props){
+    console.log(props)
+  }
 
 
 
-class Footer extends React.Component{
-  render(){
-    return(
-      <div className="footer">
-        <h3>
-          Footer
-        </h3>
+  render() {
+    return (
+      <div>
+        <Header get_props={this.get_props} name={this.state.name} email={this.state.email}/>
+        <Header2 />
+        <h2>My name is {this.state.name}</h2>
+        <h2>My name is {this.state.email}</h2>
+        <input onChange={(e)=>{this.handleChange(e)}} name="name" type="text" placeholder="Name" />
+        <input onChange={(e)=>{this.handleChange(e)}} name="email" type="text" placeholder="email" />
+        <button onClick={this.getState}>Get State</button>
+        <button onClick={this.set_state}>Set State</button>
+        
+        <Footer />
       </div>
     )
   }
 }
 
 
-
+class Footer extends React.Component {
+  render() {
+    return (
+      <div className="footer">
+        <h3>Footer</h3>
+      </div>
+    )
+  }
+}
 export default App;
--- src/components/Header.js
+++ src/components/Header.js
@@ -3,9 +3,8 @@ class Header extends React.Component{
     render(){
         return(
             <div className="header">
-                <h1>
-                Header
-                </h1>
+                <h1>Header {this.props.name} {this.props.email}</h1>
+                <button onClick={()=>{this.props.get_props("sylani")}}>Get Porps</button>
             </div>
         )
     }
@@ -15,16 +14,12 @@ class Header2 extends React.Component{
     render(){
         return(
             <div>
-                Body Header
+                <h4>Header 2</h4>
             </div>
-            
         )
     }
 }
 
-
-
-// export default Header
 export{
     Header,
     Header2
